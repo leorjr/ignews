eda59d07e258ebf517c925aa51adc2c4ce10c59b
Leonardo Rodrigues <leonardorodrigues@Air-de-Leonardo.local>
2022-04-27 14:41:07 -0300

    feat: created page home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
-      <title>inicio | ig.news</title>
-      <h1>Hello World</h1>
+      <title>home | ig.news</title>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 / month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
