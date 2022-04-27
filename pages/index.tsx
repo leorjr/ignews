5454645eaa3bb22b37b84ea43d2ca93b242d15f6
Leonardo Rodrigues <leonardorodrigues@Air-de-Leonardo.local>
2022-04-26 22:04:34 -0300

    feat: add title on page, like documentation talk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hellos World</h1>;
+  return (
+    <>
+      <title>inicio | ig.news</title>
+      <h1 className={styles.title}>Hellos World</h1>
+    </>
+  );
 }
